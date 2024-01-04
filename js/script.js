0c0c4825f3aa54fa0516a322d792ee6792e11b00
Sresta Bhattarai <SrestaBhattarai@Srestaas-Mac-Air.local>
2024-01-04 16:32:05 +0545

    Updates in About us page

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".slide-content", {   
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    758: {
+      slidesPerView: 2,
+    },
+    978: {
+      slidesPerView: 3,
+    },
+  },
+});
